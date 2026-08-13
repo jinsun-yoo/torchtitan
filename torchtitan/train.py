46e3b8b21e1f187867c06caa15a436714194e63a
Jinsun Yoo <jinsunyoo332@gmail.com>
2026-08-13 05:25:07 +0200

    Revive memory profiling (why was it removed in first place?)

--- torchtitan/train.py
+++ torchtitan/train.py
@@ -655,8 +655,8 @@ class Trainer(torch.distributed.checkpoint.stateful.Stateful):
                 # signal the profiler that the next profiling step has started
                 if torch_profiler:
                     torch_profiler.step()
-                # if memory_profiler:
-                #     memory_profiler.step()
+                if memory_profiler:
+                    memory_profiler.step()
 
                 # reduce timeout after first train step for faster signal
                 # (assuming lazy init and compilation are finished)
